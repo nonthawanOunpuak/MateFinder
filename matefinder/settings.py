e6206c1a3da5ab0d331dec788907813885cf527a
mai <kanaporn.inc@gmail.com>
2020-10-13 21:56:09 +0700

    updated db

--- matefinder/settings.py
+++ matefinder/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'matefinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgresql',
+        'USER': 'postgresql',
+        'PASSWORD': 'postgresql',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
