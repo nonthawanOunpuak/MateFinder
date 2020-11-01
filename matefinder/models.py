41cabcf94ba18038959fd63c9a62b42d4defeb86
mai <kanaporn.inc@gmail.com>
2020-11-01 18:19:41 +0700

    updated model

---
+++ matefinder/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Student(models.Model):
+    username = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}, {self.username}, {self.name}, {self.email}, {self.phone}, {self.year}"
+
+
+class RequestInformation(models.Model):
+    username = models.CharField(max_length=255)
+    name_req: models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.id}, {self.username}, {self.name_req}"
+
+
+class SentRequestInformation(models.Model):
+    username = models.CharField(max_length=255)
+    name_sent: models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.id}, {self.username}, {self.name_sent}"
+
+
+class DormInformation(models.Model):
+    username = models.CharField(max_length=255)
+    name_dorm = models.CharField(max_length=255)
+    details_dorm = models.CharField(max_length=255)
+    type_dorm = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}, {self.username}, {self.name_dorm}, {self.details_dorm}, {self.type_dorm}, {self.price}"
+
+
+class CheckLists(models.Model):
+    username = models.CharField(max_length=255)
+    light = models.BooleanField()
+    timetosleep = models.CharField(max_length=255)
+    pet = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.username}, {self.light}, {self.timetosleep}, {self.pet}"
--- matefinder/views.py
+++ matefinder/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .models import Student, RequestInformation, SentRequestInformation, DormInformation, CheckLists
+from django.contrib import messages
 
 
 def about(request):
@@ -8,5 +13,36 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
-# def aboutMe(request):
-#     return render(request, 'aboutme.html')
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "index.html")
+
+
+def login_view(request):
+    # if not request.user.is_authenticated:
+    #     return HttpResponseRedirect(reverse("index"))
+    # else:
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html", {
+                "message": "กรุณากรอกรหัสผ่านที่ถูกต้อง"
+            })
+    return render(request, "login.html")
+
+
+def logout_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        logout(request)
+        return render(request, "users/login.html", {
+            "message": "Logged out"
+        })
