ec4b147ba56035cbc4d198862d909957ceba1a38
nonthawanOunpuak <nonthawan.oun@dome.tu.ac.th>
2020-11-14 18:43:24 +0000

    add test

--- matefinder/settings.py
+++ matefinder/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'matefinder'
 ]
 
-# Use nose to run all tests
+# # Use nose to run all tests
 # TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # # Tell nose to measure coverage on the 'foo' and 'bar' apps
--- matefinder/tests.py
+++ matefinder/tests.py
@@ -1,31 +1,47 @@
 from django.test import TestCase
 from django.test import Client, TestCase
-from .models import Student, RequestInformation, SentRequestInformation, DormInformation,CheckLists
+from .models import Student, RequestInformation, SentRequestInformation, DormInformation
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth import authenticate, login, logout
+# from selenium import webdriver
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from selenium.webdriver.firefox.webdriver import WebDriver
+# from django_selenium_test import selenium, SeleniumTestCase, PageElement
+# from selenium.webdriver.common.by import By
 
 class UserTestCase(TestCase):
 
+
+
     def setUp(self):
 
+        # super().setUpClass()
+        # self.selenium.implicitly_wait(10)
+        # self.selenium = webdriver.Chrome()
+
         Student.objects.create(username="nonthawan1", name="nonthawan1",password="123456789", email= "2563@mail.com", phone="0123456789",year = 1)
+        Student.objects.create(username="nonthawan2", name="nonthawan2",password="123456789", email= "2563@mail.com", phone="0123456789",year = 1)
         RequestInformation.objects.create(username="nonthawan1", name_req="12345")
         SentRequestInformation.objects.create(username="nonthawan1", name_sent="nonthawan1")
-        DormInformation.objects.create(username="nonthawan1", name_dorm="1234", details_dorm="123",type_dorm="1123",price=4000)
-        CheckLists.objects.create(username="nonthawan1", light=True,timetosleep="12",pet=True)
+        self.user = DormInformation.objects.create(username="nonthawan1", name_dorm="1234", details_dorm="123",type_dorm="1123",price=4000,light=True,timetosleep="1 a.m",pet=True)
 
         # Create User
-        self.user1 = User.objects.create_user(username="knanporn",password="Knanporn1",email="knanporn@mail.com")
+        self.user1 = User.objects.create_user(username="nonthawan1",password="Knanporn1",email="3591@mail.com")
 
         self.about = reverse("about")
         self.signup = reverse("signup")
         self.home = reverse("home")
         self.logout = reverse("logout")
         self.login = reverse("login")
+        self.post = reverse("post")
+        self.store = reverse("store")
+        self.homepage = reverse("homepage")
 
     # Django Testing
+    # def test_delete_post(self):
+
 
     # Client Testing
 
@@ -101,3 +117,29 @@ class UserTestCase(TestCase):
         response = c.get(self.logout)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response , 'login.html')
+
+    def test_add_post(self):
+        c = Client()
+        c.force_login(self.user1)
+        response = c.post(self.home)
+        self.assertEqual(response.status_code, 200)
+        response = c.post(self.post)
+        response = c.get('/post')
+        self.assertEqual(response.status_code, 200)
+        response = c.post('/post',{'username':'nonthawan','name_dorm':'city park','details_dorm':'aaaaaa','type_dorm':'man','price':7500,'light':True,'timetosleep':'1 a.m','pet':True}, follow=True)
+        response = self.client.post(self.post,{'post':'post',})
+        self.assertTemplateUsed(response , 'post.html')
+        # response = c.get(self.store)
+        # self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response , 'homepage.html')
+        # self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        c = Client()
+        c.force_login(self.user1)
+        response = c.post(self.homepage)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["dorms"],self.user)
+
+
+
